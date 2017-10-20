534d300e15f790e26696f2eb08baa5cd7a2193ea
Kody Ellis <klouds64@gmail.com>
2017-10-20 18:38:52 -0500

    Update ke_lab6.py
    made various changes

--- ke_lab6.py
+++ ke_lab6.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Oct 20 16:21:56 2017
-letterCount
-
 @author: Kody Ellis
+Prints the amount of time each letter of the alphabet occurs in a file.
 """
 
 import string
-alphaString = string.lowercase
+
+#Makes a variable that has all of the lower case alphabet
+alphaStringLow = string.lowercase
+
 
 #function returns the value of the number of time an alphabet letter occurs in a file
 ##Test file provided is gettysburg.txt
@@ -24,15 +26,17 @@ def letterCount(target_char, file_name):
     #traverses each character in the file
     for char in data:
         #Sees if the current chracters selected is the chracter we are counting right now.
-        if char == target_char:
+        #Counts the lower or upper case occurance of the target character
+        if char == target_char or char.upper() == target_char:
             number_of_occurences += 1
-        
+            
     #Closes file
     file_connector.close()
     
     return number_of_occurences
     
+#Prints a newline for aesthetic reasons
 print '\n'
 
-for char in alphaString:
+for char in alphaStringLow:
     print char, "=", letterCount(char, 'gettysburg.txt')
